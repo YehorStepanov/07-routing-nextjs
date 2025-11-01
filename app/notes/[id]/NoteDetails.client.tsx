8a9c8b036b5b78a073ab861c00c5554275caf8c3
Yehor Stepanov <stepegorr@gmail.com>
2025-11-01 16:21:29 +0100

    fix 5

---
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+import NotePreview from '@/components/NotePreview/NotePreview';
+import { Note } from '@/types/note';
+
+interface Props {
+  note: Note;
+}
+
+export default function NoteDetailsClient({ note }: Props) {
+  return <NotePreview note={note} />;
+}
